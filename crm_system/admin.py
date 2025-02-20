ccb3974456a4c3ed343c9e5cb852d3f87bd0165c
maichu <chuthimai18245@gmail.com>
2025-02-20 20:11:44 +0700

    thêm admin

--- crm_system/admin.py
+++ crm_system/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Customer, Product, Employee, TaskBoard
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone')
+    search_fields = ('name', 'email')
+    list_filter = ('created_at',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'stock')
+    search_fields = ('name',)
+    list_filter = ('price',)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'position', 'email')
+    search_fields = ('name', 'position')
+    list_filter = ('position',)
+
+
+@admin.register(TaskBoard)
+class TaskBoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'assigned_to')
+    search_fields = ('title',)
+    list_filter = ('status',)
